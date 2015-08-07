53788ac34b18f28345e1b168e853050d888f851a
Dean Welch <deanwelch286@gmail.com>
2015-08-07 20:03:55 +0100

    database models created and registered on the admin page

--- gameRecommender/admin.py
+++ gameRecommender/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import GameInfo, GameFeatures, GameTags
+
+admin.site.register(GameInfo)
+admin.site.register(GameFeatures)
+admin.site.register(GameTags)
--- gameRecommender/models.py
+++ gameRecommender/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+class GameTags(models.Model):
+    tags = models.CharField(unique=True, max_length=255)
+
+
+class GameFeatures (models.Model):
+    features = models.CharField(unique=True, max_length=255)
+
+
+class GameInfo(models.Model):
+    game_name = models.CharField(max_length=255)
+    app_ID = models.PositiveIntegerField(unique=True)
+    metascore = models.PositiveIntegerField()
+    positive_review_numbers = models.PositiveIntegerField()
+    negative_review_numbers = models.PositiveIntegerField()
+    picture = models.URLField()
+    gameTags = models.ManyToManyField(GameTags)
+    gameFeatures = models.ManyToManyField(GameFeatures)
