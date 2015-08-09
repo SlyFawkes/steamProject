beb7f27a1b3f3037d857279656b585430616a5e4
Dean Welch <deanwelch286@gmail.com>
2015-08-09 21:06:54 +0100

    homepage now shows a table of current games

--- gameRecommender/views.py
+++ gameRecommender/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.template.context import RequestContext
 from django.http import HttpResponse
 from .models import GameInfo
 
 
 def index(request):
-    game_list = GameInfo.objects.order_by('app_ID')[:]
-    context = {'game_list': game_list}
+    #game_list = GameInfo.objects.order_by('app_ID')[:]
+    context = {'game_list': GameInfo.objects.all()}
     return render(request, 'gameRecommender/index.html', context)
