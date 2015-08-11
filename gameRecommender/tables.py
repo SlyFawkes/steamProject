36c103f3535751651531fe6535b3f83836877aa7
Dean Welch <deanwelch286@gmail.com>
2015-08-11 08:15:58 +0100

    table now sort of displays game tags, sorting by the game tags though doesn't work

--- gameRecommender/tables.py
+++ gameRecommender/tables.py
@@ -3,7 +3,23 @@ __author__ = 'Dean'
 import django_tables2 as tables
 from gameRecommender.models import GameInfo
 
+
 class GameTable(tables.Table):
+
+    game_tags_column = tables.Column(empty_values=(), accessor='gameTags')
+
+    def render_game_tags_column(self, record):
+
+        data = []
+        game_id = record.app_ID
+        game = GameInfo.objects.get(app_ID=game_id)
+        tags = game.gameTags.filter().values('tags')
+
+        for item in tags:
+            data.append(item['tags'])
+
+        return data
+
     class Meta:
         model = GameInfo
         # add class="paleblue" to <table> tag
