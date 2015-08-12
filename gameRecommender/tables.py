4fefa124b4dd3fe5706420a45c696aec2c4c5225
Dean Welch <deanwelch286@gmail.com>
2015-08-12 23:30:58 +0100

    game tags and features are now displayed, sorting is still partially broken

--- gameRecommender/tables.py
+++ gameRecommender/tables.py
@@ -7,16 +7,29 @@ from gameRecommender.models import GameInfo
 class GameTable(tables.Table):
 
     game_tags_column = tables.Column(empty_values=(), accessor='gameTags')
+    game_features_column = tables.Column(empty_values=(), accessor='gameFeatures')
 
     def render_game_tags_column(self, record):
 
-        data = []
+        data = ''
         game_id = record.app_ID
         game = GameInfo.objects.get(app_ID=game_id)
         tags = game.gameTags.filter().values('tags')
 
         for item in tags:
-            data.append(item['tags'])
+            data += item['tags'] + ' '
+
+        return data
+
+    def render_game_features_column(self, record):
+
+        data = ''
+        game_id = record.app_ID
+        game = GameInfo.objects.get(app_ID=game_id)
+        features = game.gameFeatures.filter().values('features')
+
+        for item in features:
+            data += item['features'] + ' '
 
         return data
 
